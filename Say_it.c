a2f80ee85f44c09bd11108ea1a8b54b9d18a675f
Mohammad Sayeed Ahmed Ovi <ovi11092000@gmail.com>
2025-10-29 00:19:57 +0600

    Add files via upload

---
+++ Say_it.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); 
+
+    int i = 1;
+    while(i <= N) {  
+        printf("%d. I Want More Assignments\n", i);
+        i++;         
+    }
+
+    return 0;
+}
