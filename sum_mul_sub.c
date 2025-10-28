35683f83b18b63e04875a5468a2996505f31db50
Mohammad Sayeed Ahmed Ovi <ovi11092000@gmail.com>
2025-10-29 00:05:50 +0600

    Add files via upload

---
+++ sum_mul_sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   long long int X, Y;
+    scanf("%lld %lld", &X, &Y);
+    long long int summation=X+Y;
+    long long int multiplication=X*Y;
+    long long int subtraction=X-Y;
+
+    printf("%lld + %lld = %lld\n", X, Y, summation);
+    printf("%lld * %lld = %lld\n", X, Y, multiplication);
+    printf("%lld - %lld = %lld\n", X, Y, subtraction);
+
+    return 0;
+}
